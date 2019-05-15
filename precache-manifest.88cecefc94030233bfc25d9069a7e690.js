77ecb5b2b18ba262a7eacbd4dd13b066ce5850a9
Nikola Bojanovic <nikolabojanovicmob@gmail.com>
2019-05-15 22:16:46 +0200

    Updates

---
+++ precache-manifest.88cecefc94030233bfc25d9069a7e690.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fd8d680f6345a89b70e80d8a4f889d7",
+    "url": "/blogger/index.html"
+  },
+  {
+    "revision": "68457ca1f1d7ef2273cb",
+    "url": "/blogger/static/css/2.1f156bdb.chunk.css"
+  },
+  {
+    "revision": "c532ab744b58ee8d472c",
+    "url": "/blogger/static/css/main.e259570c.chunk.css"
+  },
+  {
+    "revision": "68457ca1f1d7ef2273cb",
+    "url": "/blogger/static/js/2.f582a765.chunk.js"
+  },
+  {
+    "revision": "c532ab744b58ee8d472c",
+    "url": "/blogger/static/js/main.9fcc0aa1.chunk.js"
+  },
+  {
+    "revision": "1ba68dc7c4d4cffeadb2",
+    "url": "/blogger/static/js/runtime~main.669a5cdf.js"
+  }
+]);
